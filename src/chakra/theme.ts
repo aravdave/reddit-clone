4b0049f4a6bcde527fa76f9a4b078c9b9f124c0e
aravdave <aravdave@gmail.com>
2023-04-07 15:59:11 -0400

    Add Reddit icons, working on NavBar

--- src/chakra/theme.ts
+++ src/chakra/theme.ts
@@ -1,12 +1,13 @@
 import "@fontsource/open-sans/300.css";
 import "@fontsource/open-sans/400.css";
 import "@fontsource/open-sans/700.css";
-import { extendTheme } from '@chakra-ui/react';
+import { extendTheme } from "@chakra-ui/react";
+import { Button } from "./button";
 
 export const theme = extendTheme({
   colors: {
     brand: {
-      100: "#FF3c00"
+      100: "#FF3c00",
     },
   },
   fonts: {
@@ -17,9 +18,9 @@ export const theme = extendTheme({
       body: {
         bg: "gray.200",
       },
-    })
+    }),
   },
   components: {
-    // Button
-  }
+    Button,
+  },
 });
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,26 @@
-import { Flex } from '@chakra-ui/react'
-import React, { ReactNode } from 'react'
+import { Flex, Image } from "@chakra-ui/react";
+import React, { ReactNode } from "react";
+import SearchInput from "./SearchInput";
+import RightContent from "./RightContent/RightContent";
 
 type Props = {
-  children?: ReactNode
-}
+  children?: ReactNode;
+};
 
 export default function Navbar({ children }: Props) {
   return (
-    <Flex bg="white" height='44px' padding="6px 12px">
-      <Flex>Logos</Flex>
+    <Flex bg="white" height="44px" padding="6px 12px">
+      <Flex align="center">
+        <Image src="/images/redditFace.svg" height="30px" />
+        <Image
+          src="/images/redditText.svg"
+          height="46px"
+          display={{ base: "none", md: "unset" }}
+        />
+      </Flex>
+      {/* <Directory /> */}
+      <SearchInput />
+      <RightContent />
     </Flex>
-  )
+  );
 }
